cd142a4467e2def1e1d05a5b719c0d441d85b5e0
razikusman <razikusman@gmail.com>
2021-05-01 06:47:01 +0530

    quize 27

---
+++ src/quizeTwentySeven/q6.java
@@ -0,0 +1,20 @@
+package quizeTwentySeven;
+
+public class q6 {
+    public static void main(String[] args) {
+        int[][] table = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        }; 
+                
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                System.out.print(table[j][i]);
+            }
+            //147258369
+        }
+    }
+    
+    
+}

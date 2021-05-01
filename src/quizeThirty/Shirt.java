2e61d121756a96461c2fa7fdf9b165f98c76d7a2
razikusman <razikusman@gmail.com>
2021-05-01 07:49:17 +0530

    quize 30

---
+++ src/quizeThirty/Shirt.java
@@ -0,0 +1,23 @@
+package quizeThirty;
+
+public class Shirt {
+ 
+    private String brand;
+    private double price;
+    private String size;
+ 
+    // constructor ...
+ 
+    public Shirt(String string, double d, String string2) {
+    }
+    public String getBrand() {
+        return this.brand;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+    public String getSize() {
+        return this.size;
+    }
+
+}
---
+++ src/quizeThirty/q1.java
@@ -0,0 +1,16 @@
+package quizeThirty;
+
+public class q1 {
+    public static void main(String[] args) {
+
+        Shirt nike = new Shirt("Nike", 20.99, "Small");
+        
+        System.out.println("Brand: " + nike.getBrand());
+        System.out.println("Price: " + nike.getPrice());
+        System.out.println("Size: " + nike.getSize());;
+ 
+    }
+}
+
+
+
